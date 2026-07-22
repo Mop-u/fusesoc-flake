e1707d7438fbfb41e6f589300daab62a7af331ad
Quinn Unger <quinn.unger@pm.me>
2026-07-22 13:56:13 +0100

    add more cores from the fusesoc-cores repo (#32)

--- flake.nix
+++ flake.nix
@@ -37,7 +37,29 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
           inherit (self.packages.${system}) fusesoc;
-          coreList = pkgs.callPackage ./cores/fusesoc-cores.nix { fusesocLib = fusesoc.lib; };
+          coreList =
+            (pkgs.callPackage ./cores/fusesoc-cores.nix { fusesocLib = fusesoc.lib; })
+            ++ (fusesoc.lib.importCores (
+              pkgs.fetchFromGitHub {
+                name = "openrisc-cores-github";
+                owner = "openrisc";
+                repo = "openrisc-cores";
+                rev = "b87ad6a7799c996814200c51a7f58d27ab93818f";
+                hash = "sha256-R7KWnrerYV4ZA+NE6z4yWS9fgfKyUxP/LoBB0z0EOk8=";
+              }
+            ))
+            ++ [
+              (fusesoc.lib.importCore {
+                coreName = "elf-loader.core";
+                coreRoot = pkgs.fetchFromGitHub {
+                  name = "elf-loader-github";
+                  owner = "fusesoc";
+                  repo = "elf-loader";
+                  rev = "7a8f63ae203cb8eef0cc0b96c52a1056ae2a423e";
+                  hash = "sha256-ABzox3UanVqTJSuXuT4IEyxYQuvAxCvpzJiuaXZu8Co=";
+                };
+              })
+            ];
         in
         {
           # Flakes that export cores should extend/override legacyPackages.${system}.fusesocCores
@@ -55,7 +77,7 @@
               let
                 pkg = pkgs.fusesoc.override {
                   python3Packages = pkgs.python3Packages // {
-                    inherit (self.packages.${system}) edalize;
+                    inherit (moppkgs.packages.${system}) edalize;
                   };
                 };
               in
@@ -85,35 +107,14 @@
           checks =
             let
               noVendor = fusesoc.lib.cores.""."";
-              inherit (fusesoc.lib.cores) bespoke-silicon-group;
+              inherit (fusesoc.lib) cores;
             in
             {
-              inherit (noVendor)
-                SD-card-controller
-                ac97
-                altera_virtual_jtag
-                verilog-arbiter
-                cdc_utils
-                ethmac
-                fplib
-                jtag_tap
-                ompic
-                ;
-              bsg-basejump_stl-hard = bespoke-silicon-group.basejump_stl.hard;
-              bsg-basejump_stl-nonsynth = bespoke-silicon-group.basejump_stl.nonsynth;
-              bsg-basejump_stl-rtl = bespoke-silicon-group.basejump_stl.rtl;
-              bsg-external-hardfloat = fusesoc.lib.cores.bsg-external.hardfloat."0.0.1"; # original corename is mangled like this
-              jtag_vpi_0-r3 = noVendor.jtag_vpi."0-r3";
-              jtag_vpi_0-r4 = noVendor.jtag_vpi."0-r4";
-              jtag_vpi_0-r5 = noVendor.jtag_vpi."0-r5";
-              "mor1kx_5.0-r2" = noVendor.mor1kx."5.0-r2";
-              "mor1kx_5.1" = noVendor.mor1kx."5.1";
-              "mor1kx_5.2" = noVendor.mor1kx."5.2";
-              # Runnable testbenches
-              # SD-card-controller-lint = noVendor.SD-card-controller.run.lint.withTools [ pkgs.verilator ];
-              # ac97-sim = noVendor.ac97.run.sim.withTools [ pkgs.iverilog ];
+              inherit (self.packages.${system}) default;
+
+              # Runnable flows
               ethmac-lint = noVendor.ethmac.run.lint.withTools [ pkgs.verilator ];
-              adv_debug_sys = noVendor.adv_debug_sys.run.jsp_tb.withTools [ pkgs.iverilog ];
+              adv_debug_sys-jsp_tb = noVendor.adv_debug_sys.run.jsp_tb.withTools [ pkgs.iverilog ];
               inherit ((noVendor.fifo.withTools [ pkgs.iverilog ]).run)
                 fifo_fwft_tb
                 dual_clock_fifo_tb
@@ -121,6 +122,16 @@
                 ;
               "i2c_1.15-sim" = noVendor.i2c."1.15".run.sim.withTools [ pkgs.iverilog ];
               "i2c_1.14-r1-sim" = noVendor.i2c."1.14-r1".run.sim.withTools [ pkgs.iverilog ];
+              servant-tinyfpga_bx = noVendor.servant.run.tinyfpga_bx.withTools [
+                pkgs.icestorm
+                pkgs.yosys
+                pkgs.nextpnr
+              ];
+              blinky-tinyfpga_bx = cores.fusesoc.utils.blinky.run.tinyfpga_bx.withTools [
+                pkgs.icestorm
+                pkgs.yosys
+                pkgs.nextpnr
+              ];
             };
         }
       );
